5fc7e633621317f38e6cc46889021d74aa81c562
karinamina3 <00012217@uca.edu.sv>
2021-07-03 16:44:38 -0600

    Validaciones y boton back funcional. bug fixes

--- finish/src/main/java/org/tensorflow/lite/examples/classification/activities/DogActivity.kt
+++ finish/src/main/java/org/tensorflow/lite/examples/classification/activities/DogActivity.kt
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.EditText
-import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Timestamp
@@ -17,54 +16,62 @@ import java.util.*
 
 class DogActivity : AppCompatActivity() {
 
-    val db=FirebaseFirestore.getInstance()
+    private val db = FirebaseFirestore.getInstance()
+    private val myCalendar = Calendar.getInstance()
+    private lateinit var sdf: SimpleDateFormat
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog)
 
         val razaLabel = "Raza 🐶: " + intent.getStringExtra("raza")
         razaPerro.text = razaLabel
-        val dateInput=findViewById<EditText>(R.id.dateInput)
-        
+        val dateInput = findViewById<EditText>(R.id.dateInput)
+
+        val myFormat = "dd/MM/yy"
+        sdf = SimpleDateFormat(myFormat, Locale.US)
         initControls(dateInput)
+        dateInput.setText(sdf.format(myCalendar.time))
 
-
-        //val selectedRadioButtonId: Int = radioGroup.checkedRadioButtonId
-       //val selectedRadioButton = findViewById<RadioButton>(selectedRadioButtonId)
-        //val string: String = selectedRadioButton.text.toString()
-
-
+        backButton.setOnClickListener { finish() }
         checkButton.setOnClickListener {
-            //TODO: NOT QUITE WORKING
-            var optionGender: String = "Macho"
+            if (ValidadorCampos()) {
+                var optionGender = "Macho"
 
-            if (femaleOption.isSelected && !maleOption.isSelected) {
-                optionGender = "Hembra";
-            } else if (!femaleOption.isSelected && maleOption.isSelected) {
-                optionGender = "Macho"
-            };
-
-            db.collection("Perros").add(Dog(idRaza = intent.getStringExtra("idRaza") ,name = nombreInput.text.toString(),fechaRescate = Timestamp(Date()),peso = pesoInput.text.toString(), sexo = optionGender  )).addOnSuccessListener { newDoc ->
-
-                Toast.makeText(this, "Creado con exito", Toast.LENGTH_SHORT).show()
-            }
-                .addOnFailureListener {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                if (femaleOption.isChecked && !maleOption.isChecked) {
+                    optionGender = "Hembra"
+                } else if (!femaleOption.isChecked && maleOption.isChecked) {
+                    optionGender = "Macho"
                 }
 
-        }
+                db.collection("Perros").add(
+                    Dog(
+                        idRaza = intent.getStringExtra("idRaza"),
+                        name = nombreInput.text.toString(),
+                        fechaRescate = Timestamp(myCalendar.time),
+                        peso = pesoInput.text.toString(),
+                        sexo = optionGender
+                    )
+                ).addOnSuccessListener {
+                    Toast.makeText(this, "Creado con exito", Toast.LENGTH_SHORT).show()
+                    finish()
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
 
+                }
+            } else {
+                Toast.makeText(this, "Campos deben estar llenos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun ValidadorCampos(): Boolean {
+        return nombreInput.text!!.isNotEmpty() && pesoInput.text!!.isNotEmpty() && dateInput.text!!.isNotEmpty() && (femaleOption.isChecked || maleOption.isChecked)
     }
 
 
-    private fun initControls(dateInput:EditText) {
-        val myCalendar = Calendar.getInstance()
-
-        val myFormat = "dd/MM/yy"
-        val sdf = SimpleDateFormat(myFormat, Locale.US)
+    private fun initControls(dateInput: EditText) {
 
         val dogDate: DatePickerDialog.OnDateSetListener =
             DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
@@ -86,7 +93,6 @@ class DogActivity : AppCompatActivity() {
                 myCalendar[Calendar.DAY_OF_MONTH]
             )
 
-            dialog.datePicker.minDate = Date().time
             dialog.setButton(
                 DialogInterface.BUTTON_NEGATIVE, "CANCEL"
             ) { _: DialogInterface?, _: Int ->
