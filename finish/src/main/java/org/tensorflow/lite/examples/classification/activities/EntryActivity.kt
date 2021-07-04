6b805ac5675cc06fd7e37e3143ffc75f84c34c08
karinamina3 <00012217@uca.edu.sv>
2021-07-03 19:07:26 -0600

    Cambiada activity entry, Rename DogoModel.tflite

---
+++ finish/src/main/java/org/tensorflow/lite/examples/classification/activities/EntryActivity.kt
@@ -0,0 +1,12 @@
+package org.tensorflow.lite.examples.classification.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.tensorflow.lite.examples.classification.R
+
+class EntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entry)
+    }
+}
